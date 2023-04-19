f3c6c9b2dff8ac368b7e5b3f065808cb2f953873
Kassim <kassimbouzoubaa05@gmail.com>
2023-04-20 01:11:31 +0200

    first commit

---
+++ src/pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel='icon' href='https://media.graphassets.com/zBLcmcqfTqWM40oqOtea?_gl=1*1x19891*_ga*NjI3OTA1ODgzLjE2ODE2NjM0MzQ.*_ga_G6FYGSYGZ4*MTY4MTk0MjcyMC42LjEuMTY4MTk0NTQzOC42MC4wLjA.' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
