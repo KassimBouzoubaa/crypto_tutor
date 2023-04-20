0c6e529e31b992322229d9034c8d22c19bfd2475
Kassim <kassimbouzoubaa05@gmail.com>
2023-04-20 03:43:58 +0200

    ajout

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,6 +11,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel='icon' href='https://media.graphassets.com/zBLcmcqfTqWM40oqOtea?_gl=1*1x19891*_ga*NjI3OTA1ODgzLjE2ODE2NjM0MzQ.*_ga_G6FYGSYGZ4*MTY4MTk0MjcyMC42LjEuMTY4MTk0NTQzOC42MC4wLjA.' />
+          <title>Crypto Tutor</title>
         </Head>
         <body>
           <Main />
